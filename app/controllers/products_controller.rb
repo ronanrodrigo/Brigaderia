7b3945abed2a576868aced2464a170c93d12a87c
Thiago A. Klein <klein.thiago@gmail.com>
2011-09-20 18:32:55 -0300

    mundo do thiago

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_filter :authenticate_user!, :except => :client_products
+  layout "administration", :except => :client_products
   
   # GET /products
   # GET /products.json
