c22c142cc597fae816fcb13ba0d9f036db25871a
Ronan Rodrigo Nunes <ronanchiwi@gmail.com>
2011-10-12 15:34:33 -0300

    pe de mula

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :generics
+  def generics
+    featureds = Product.where(:featured => true)
+    @featured_products = featureds.blank? ? simples.limit(5) : featureds.limit(5)
+  end
 end
