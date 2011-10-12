4a7a265eea2476d0244c66c1df8718094e5de16c
Ronan Rodrigo Nunes <ronanchiwi@gmail.com>
2011-10-12 15:46:29 -0300

    simples

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :generics
   def generics
     featureds = Product.where(:featured => true)
+    simples = Product.where(:featured => false)
     @featured_products = featureds.blank? ? simples.limit(5) : featureds.limit(5)
   end
 end
